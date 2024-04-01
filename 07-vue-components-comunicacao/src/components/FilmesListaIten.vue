7bb03a7622d658a4bf10f3f940d22db709f702c1
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-03-31 22:20:04 -0300

    07-vue-components-comunicacao - Comunicação entre components irmãos, Centralizando comunicação com Event Bus. Reaproveitando código e dados com Event Bus

--- 07-vue-components-comunicacao/src/components/FilmesListaIten.vue
+++ 07-vue-components-comunicacao/src/components/FilmesListaIten.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import { eventBus } from '@/main'
+
 export default {
   props: {
     filme: {
@@ -15,7 +17,9 @@ export default {
   },
   methods: {
     selecionar (event) {
-      this.$emit('selecionarFilme', this.filme)
+      // this.$emit('selecionarFilme', this.filme)
+      // eventBus.$emit('selecionarFilme', this.filme)
+      eventBus.selecionarFilme(this.filme)
     }
   }
 }
