46557fcbed32e8368c4f0633c7adfd03947683d1
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-08 20:22:26 -0300

    08-slots-components-dinamicos-e-components-assincronos -  Manipulando estado de carregamento de Components Assíncronos, Bônus propriedade name nos Components

--- 08-slots-components-dinamicos-e-components-assincronos/src/App.vue
+++ 08-slots-components-dinamicos-e-components-assincronos/src/App.vue
@@ -6,6 +6,7 @@
     <button @click="componentSelecionado = 'Sobre'">Sobre</button>
     <button @click="componentSelecionado = 'PostsLista'">Posts</button>
     <button @click="componentSelecionado = 'AssincronoTester'">Assincrono</button>
+    <button @click="componentSelecionado = 'Contato'">Contato</button>
 
     <KeepAlive :include="['Sobre']">
       <component :is="componentSelecionado" v-bind="propsAtuais"></component>
@@ -18,12 +19,31 @@ import Home from './components/PageHome.vue'
 import Sobre from './components/PageSobre.vue'
 import PostsLista from './components/PostsLista.vue'
 
+const Contato = {
+  render: (h) => h({
+    name: 'ContatoDados',
+    template: '<h2>Component Anonimo</h2>'
+  })
+}
+
 export default {
   components: {
     Home,
     Sobre,
     PostsLista,
-    AssincronoTester: () => import('./components/AssincronoTester.vue')
+    AssincronoTester: () => ({
+      component: new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(import('./components/AssincronoTester.vue'))
+          reject('Carregando falhou')
+        }, 2000)
+      }),
+      loading: { template: '<p>Carregando...</p>' },
+      error: { template: '<p>Erro ao carregar componente!</p>' },
+      delay: 200,
+      timeout: 3000
+    }),
+    Contato
   },
   data() {
     return {
