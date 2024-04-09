11033cd42f3aec68598238d27b1aefc68ad706bc
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-08 23:32:23 -0300

    10-animando-elementos-e-components - Criando novo projeto

---
+++ 10-animando-elementos-e-components/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center text-body-emphasis text-decoration-none">
+            <img src="./assets/logo.png" alt="VueJS" width="32" />
+            <span class="fs-4">VueJS</span>
+          </a>
+        </header>
+
+        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Animações</h1>
+            <p class="col-md-8 fs-4">Treinando transição/animação de elementos/components no Vue.</p>
+          </div>
+        </div>
+
+        <footer class="pt-3 mt-4 text-body-secondary border-top">
+          &copy; 2024
+        </footer>
+      </div>
+    </main>
+  </div>
+</template>
