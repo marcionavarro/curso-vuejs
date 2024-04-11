b5fa7e6ef926149081e024272bfbfac51ed31c68
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-11 00:31:53 -0300

    10-animando-elementos-e-components - Gatilhos JavaScript na prática, Desativando CSS das animações, Usando JavaScript para criar animações

--- 10-animando-elementos-e-components/src/App.vue
+++ 10-animando-elementos-e-components/src/App.vue
@@ -18,8 +18,10 @@
 
         <div class="container">
           <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alterar</button>
-          <div id="example-3">
-            <transition enter-active-class="animated bounceInLeft" leave-active-class=" animated bounceOutDown">
+          <div>
+            <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+              @enter-cancelled="enterCancelled" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave"
+              @leave-cancelled="leaveCancelled" :css="false">
               <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
             </transition>
           </div>
@@ -38,6 +40,61 @@ export default {
   data () {
     return {
       mostrar: true
+    }
+  },
+  methods: {
+    beforeEnter (el) {
+      console.log('beforeEnter')
+      el.style.opacity = 0
+    },
+    enter (el, done) {
+      console.log('enter')
+
+      let contagem = 0
+
+      const intervalo = setInterval(() => {
+        el.style.opacity = +el.style.opacity + 0.1
+        contagem++
+        if (contagem > 10) {
+          clearInterval(intervalo)
+          done()
+        }
+      }, 150)
+    },
+    afterEnter (el) {
+      console.log('afterEnter')
+    },
+    enterCancelled (el) {
+      console.log('enterCancelled')
+    },
+
+    beforeLeave (el) {
+      console.log('beforeLeave')
+      el.style.transition = 'width 0.5s'
+      el.style.overflow = 'hidden'
+      el.style.withSpace = 'nowrap'
+    },
+    leave (el, done) {
+      console.log('leave')
+
+      let contagem = 0
+      const tamanho = el.offsetWidth
+
+      const intervalo = setInterval(() => {
+        el.style.width = (tamanho * (1 - (contagem / 10))) + 'px'
+        contagem++
+
+        if (contagem > 10) {
+          clearInterval(intervalo)
+          done()
+        }
+      }, 150)
+    },
+    afterLeave (el) {
+      console.log('afterLeave')
+    },
+    leaveCancelled (el) {
+      console.log('leaveCancelled')
     }
   }
 }
