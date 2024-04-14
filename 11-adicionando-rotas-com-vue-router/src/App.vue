208652b65b15108ff77d642ec89910136432c07f
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-14 00:23:13 -0300

    11-adicionando-rotas-com-vue-router - Criando novo projeto com Vue CLI

---
+++ 11-adicionando-rotas-com-vue-router/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center text-body-emphasis text-decoration-none">
+            <img src="./assets/logo.png" alt="VueJS" width="32"/>
+            <span class="fs-4 ms-2">VueJS</span>
+          </a>
+        </header>
+
+        <div class="mb-4 bg-body-tertiary rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Vue Router</h1>
+            <p class="col-md-8 fs-4">Adicionando rotas a Single Page Applications no Vue.</p>
+          </div>
+        </div>
+
+        <div class="container">
+          <h3 class="fw-light">Contatos</h3>
+        </div>
+
+        <footer class="pt-3 mt-5 text-body-secondary border-top">
+          &copy; 2024
+        </footer>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
