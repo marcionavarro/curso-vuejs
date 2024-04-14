81f1e7d08faca7905c6d8fbebbe60a5c4b95d12b
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-04-14 00:02:02 -0300

    10-animando-elementos-e-components - Introdução a animação de estados, Preparando código para animar números, Animando troca de valores em números com Green Sock

--- 10-animando-elementos-e-components/src/App.vue
+++ 10-animando-elementos-e-components/src/App.vue
@@ -10,31 +10,19 @@
         </header>
 
         <div class="container">
-          <h3 class="font-weight-light">Tecnologias</h3>
+          <h3 class="font-weight-light">Animações de estado</h3>
 
-          <div class="row align-items-center my-4">
-            <div class="col-sm-2">
-              <button class="btn btn-outline-info btn-sm text-white" @click="embaralhar">Embaralhar</button>
-            </div>
-
-            <div class="col-sm-10">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Insira um novo item e pressione Enter"
-                  @keyup.enter="adicionar" ref="input">
-              </div>
-            </div>
+          <div class="form-group mb-3">
+            <input class="form-control" v-model="numero" step="50">
           </div>
 
-          <transition-group tag="ul" class="list-group" name="list">
-            <li class="list-group-item d-flex justify-content-between" v-for="(tecnologia, indice) in tecnologias"
-              :key="tecnologia">
-              <span>
-                {{ tecnologia }}</span>
-              <button class="btn btn-outline-danger btn-sm text-white" @click="remover(indice)">
-                x
-              </button>
-            </li>
-          </transition-group>
+          <div class="alert alert-info">
+            <h3 class="font-weight-light">
+              <strong>Número: </strong>
+              <span>{{ numeroAnimado }}</span>
+            </h3>
+          </div>
+
         </div>
 
         <footer class="pt-3 mt-5 text-body-secondary border-top">
@@ -46,51 +34,25 @@
 </template>
 
 <script>
-import { shuffle } from 'lodash'
+import { TweenLite } from 'gsap'
 
 export default {
   data () {
     return {
-      tecnologias: [
-        'javaScript',
-        'VueJs',
-        'Vuex',
-        'Vue Router'
-      ]
+      numero: 0,
+      numeroInterpolado: 0
     }
   },
-  methods: {
-    adicionar (event) {
-      const novoItem = event.target.value
-      const indice = Math.floor(Math.random() * this.tecnologias.length)
-      this.tecnologias.splice(indice, 0, novoItem)
-      this.$refs.input.value = ''
-    },
-    remover (indice) {
-      this.tecnologias.splice(indice, 1)
-    },
-    embaralhar (event) {
-      this.tecnologias = shuffle(this.tecnologias)
+  computed: {
+    numeroAnimado () {
+      return this.numeroInterpolado.toFixed(0)
+    }
+  },
+  watch: {
+    numero (novoNumero, antigoNumero) {
+      TweenLite.to(this.$data, 2, { numeroInterpolado: novoNumero })
     }
   }
 }
+
 </script>
-
-<style scoped>
-.list-enter,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(-70px);
-}
-
-.list-enter-active,
-.list-leave-active,
-.list-move {
-  transition: all 1s;
-}
-
-.list-leave-active {
-  position: absolute;
-  width: calc(100% - 100px);
-}
-</style>
