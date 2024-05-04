357115a8caed41008e4f714d73f2d9de1af1f3bc
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-05-04 09:30:40 -0300

    13-gerenciamento-de-estado-centralizado-com-vuex (main) -  Mapeando o State via mapState, Spread Operator mesclando Computed Properties

--- 13-gerenciamento-de-estado-centralizado-com-vuex/src/components/Contador.vue
+++ 13-gerenciamento-de-estado-centralizado-com-vuex/src/components/Contador.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <h2 class="fw-lighter">Contador: {{ contador }}</h2>
+    <h4 class="fw-lighter">ContadorAlias: {{ contadorAlias }}</h4>
+    <h4 class="fw-lighter">ContadorMultiplicado: {{ contadorMultiplicado }}</h4>
     <button class="btn btn-lg btn-danger me-2" @click="decrementar">-</button>
     <button class="btn btn-lg btn-success" @click="incrementar">+</button>
   </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
-  computed: {
-    contador () {
-      return this.$store.state.contador
+  data () {
+    return {
+      contadorLocal: 2
     }
+  },
+  // computed: mapState(['contador']),
+  computed: {
+    ...mapState({
+      // contador: state => state.contador
+      contador: 'contador',
+      contadorAlias: 'contador',
+      contadorMultiplicado (state) {
+        return state.contador * this.contadorLocal
+      }
+    })
+    // outras computed properties
   },
   methods: {
     decrementar () {
