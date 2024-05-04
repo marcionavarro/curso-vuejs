39ccfd1a18ac1180410d3f8ab4cc33539e7a099b
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-05-04 08:41:24 -0300

    13-gerenciamento-de-estado-centralizado-com-vuex - First Commit

---
+++ 13-gerenciamento-de-estado-centralizado-com-vuex/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center text-body-emphasis text-decoration-none">
+            <img src="./assets/logo.png" alt="VueJS" width="32" />
+            <span class="fs-4 ms-2">VueJS</span>
+          </a>
+        </header>
+
+        <div class="mb-4 bg-body-tertiary rounded-3">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Requisições HTTP no vue</h1>
+            <p class="col-md-8 fs-4">Padrão de Gerenciamento de Estado Centralizado no Vue.</p>
+          </div>
+        </div>
+
+        <footer class="pt-3 mt-5 text-body-secondary border-top">
+          &copy; 2024
+        </footer>
+      </div>
+    </main>
+  </div>
+</template>
