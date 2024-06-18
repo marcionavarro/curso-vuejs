5f8dfaa7de8489f2c42fbdbeb85c454658f3232e
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-06-17 23:11:55 -0300

    14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql -  Populando v-selects com as listas de Contas e Categorias,  Ligações com v-model, botão de submit e cor baseada na operação, Ocultando campos tags e note e usando Tooltips, Usando modificador trim no v-model

--- 14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql/vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ 14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql/vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -7,14 +7,46 @@
       <v-flex xs12 sm6 md8 lg8>
         <v-card>
           <v-card-text>
-            <v-select name="account" label="Conta" prepend-icon="account_balance"></v-select>
-            <v-select name="category" label="Categoria" prepend-icon="class"></v-select>
-            <v-text-field name="description" label="Descrição" prepend-icon="description" type="text"></v-text-field>
-            <v-text-field name="tags" label="Tags (separadas por virgula)" prepend-icon="label"
-              type="text"></v-text-field>
-            <v-text-field name="note" label="Observação" prepend-icon="note" type="text"></v-text-field>
+            <v-form>
+              <v-select name="account" label="Conta" prepend-icon="account_balance" :items="accounts"
+                item-text="description" item-value="id" v-model="$v.record.accountId.$model"></v-select>
+              <v-select name="category" label="Categoria" prepend-icon="class" :items="categories"
+                item-text="description" item-value="id" v-model="$v.record.categoryId.$model"></v-select>
+              <v-text-field name="description" label="Descrição" prepend-icon="description" type="text"
+                v-model.trim="$v.record.description.$model"></v-text-field>
+              <v-text-field v-show="showTagsInput" name="tags" label="Tags (separadas por virgula)" prepend-icon="label"
+                type="text" v-model.trim="record.tags"></v-text-field>
+              <v-text-field v-show="showNoteInput" name="note" label="Observação" prepend-icon="note" type="text"
+                v-model.trim="record.note"></v-text-field>
+            </v-form>
+
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn icon small class="mr-3" v-on="on"><v-icon :color="color"
+                    @click="showTagsInput = !showTagsInput">label</v-icon></v-btn>
+              </template>
+              <span>Adicionar tags</span>
+            </v-tooltip>
+
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn icon small v-on="on"><v-icon :color="color"
+                    @click="showNoteInput = !showNoteInput">note</v-icon></v-btn>
+              </template>
+              <span>Observação</span>
+            </v-tooltip>
+
           </v-card-text>
         </v-card>
+
+        <v-btn color="secondary" large fab class="mt-4 mr-5" @click="$router.back()">
+          <v-icon>close</v-icon>
+        </v-btn>
+
+        <v-btn :color="color" large fab class="mt-4" @click="submit">
+          <v-icon>check</v-icon>
+        </v-btn>
+
       </v-flex>
     </v-layout>
   </v-container>
@@ -25,10 +57,15 @@ import moment from 'moment'
 import { decimal, minLength, required } from 'vuelidate/lib/validators'
 import { mapActions } from 'vuex'
 
+import AccountsService from '../services/accounts-service'
+import CategoriesService from '../services/categories-service'
+
 export default {
   name: 'RecordsAdd',
   data () {
     return {
+      accounts: [],
+      categories: [],
       record: {
         type: this.$route.query.type.toUpperCase(),
         amount: 0,
@@ -38,7 +75,9 @@ export default {
         description: '',
         tags: '',
         note: ''
-      }
+      },
+      showTagsInput: false,
+      showNoteInput: false
     }
   },
   validations: {
@@ -51,13 +90,28 @@ export default {
       description: { required, minLength: minLength(6) }
     }
   },
-  created () {
-    this.changeTitle(this.$route.query.type)
+  computed: {
+    color () {
+      switch (this.record.type) {
+        case 'CREDIT':
+          return 'primary'
+        case 'DEBIT':
+          return 'error'
+        default:
+          return 'primary'
+      }
+    }
   },
-  beforeRouteUpdate (to, from, next) {
+  async created () {
+    this.changeTitle(this.$route.query.type)
+    this.accounts = await AccountsService.accounts()
+    this.categories = await CategoriesService.categories({ operation: this.$route.query.type })
+  },
+  async beforeRouteUpdate (to, from, next) {
     const { type } = to.query
     this.changeTitle(type)
     this.record.type = type.toUpperCase()
+    this.categories = await CategoriesService.categories({ operation: type })
     next()
   },
   methods: {
@@ -76,7 +130,7 @@ export default {
       }
       this.setTitle({ title })
     },
-    log () {
+    submit () {
       console.log('Form: ', this.record)
     }
   }
