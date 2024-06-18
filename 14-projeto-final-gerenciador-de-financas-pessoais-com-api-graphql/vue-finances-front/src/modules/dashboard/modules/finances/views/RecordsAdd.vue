0b3e0dcd2f8082ee9c611d6e0609805eef09712c
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-06-17 22:21:33 -0300

    14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql - RecordsAdd Record model e validações com Vuelidate, Adicionando campos iniciais do formulário, AccountsService e Query para buscar lista de Contas, CategoriesService e Query para buscar lista de Categorias

--- 14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql/vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ 14-projeto-final-gerenciador-de-financas-pessoais-com-api-graphql/vue-finances-front/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -5,22 +5,59 @@
         <p>Amount</p>
       </v-flex>
       <v-flex xs12 sm6 md8 lg8>
-        <p>Formulário</p>
+        <v-card>
+          <v-card-text>
+            <v-select name="account" label="Conta" prepend-icon="account_balance"></v-select>
+            <v-select name="category" label="Categoria" prepend-icon="class"></v-select>
+            <v-text-field name="description" label="Descrição" prepend-icon="description" type="text"></v-text-field>
+            <v-text-field name="tags" label="Tags (separadas por virgula)" prepend-icon="label"
+              type="text"></v-text-field>
+            <v-text-field name="note" label="Observação" prepend-icon="note" type="text"></v-text-field>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import moment from 'moment'
+import { decimal, minLength, required } from 'vuelidate/lib/validators'
 import { mapActions } from 'vuex'
 
 export default {
   name: 'RecordsAdd',
+  data () {
+    return {
+      record: {
+        type: this.$route.query.type.toUpperCase(),
+        amount: 0,
+        date: moment().format('YYYY-MM-DD'),
+        accountId: '',
+        categoryId: '',
+        description: '',
+        tags: '',
+        note: ''
+      }
+    }
+  },
+  validations: {
+    record: {
+      type: { required },
+      amount: { required, decimal, different: value => value !== 0 },
+      date: { required },
+      accountId: { required },
+      categoryId: { required },
+      description: { required, minLength: minLength(6) }
+    }
+  },
   created () {
     this.changeTitle(this.$route.query.type)
   },
   beforeRouteUpdate (to, from, next) {
-    this.changeTitle(to.query.type)
+    const { type } = to.query
+    this.changeTitle(type)
+    this.record.type = type.toUpperCase()
     next()
   },
   methods: {
@@ -38,6 +75,9 @@ export default {
           title = 'Novo Lançamento'
       }
       this.setTitle({ title })
+    },
+    log () {
+      console.log('Form: ', this.record)
     }
   }
 }
